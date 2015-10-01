80df3434aba7566f990b9c7324b36c060b4dd818
kimo5532 <shotbyabel@gmail.com>
2015-10-01 10:14:19 -0700

    add emojis, crazy regex

--- chatApp/www/js/app.js
+++ chatApp/www/js/app.js
@@ -2,7 +2,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','btford.socket-io'])
+angular.module('starter', ['ionic','btford.socket-io', 'ngSanitize'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -84,7 +84,7 @@ angular.module('starter', ['ionic','btford.socket-io'])
 //|||||||||||||||||||||||||||||||||||||
 //||||| CHAT CONTROLLER|||||||||||||||||||||||||
 //|||||||||||||||||||||||||||||||||||||4. inject stateParams// inject mySocket dependency
-.controller('ChatController', function($scope, $stateParams, Socket, $ionicScrollDelegate){
+.controller('ChatController', function($scope, $stateParams, Socket, $ionicScrollDelegate, $sce){
   $scope.messages = [];//messages array
   $scope.nickname = $stateParams.nickname;
 //
@@ -113,6 +113,9 @@ var colors = ['#6CDE4D', '#CE9026', '#CE30E7', '#5E9FFE', '#38D4C8', '#D43FBF'];
   });
     //f. tell socket clien to listen for server!
     Socket.on("Message", function(data){
+
+      data.message = gimmeEmojis(data.message);
+      data.message = $sce.trustAsHtml(data.message);
       //
       $scope.messages.push(data);
       //
@@ -146,6 +149,41 @@ var colors = ['#6CDE4D', '#CE9026', '#CE30E7', '#5E9FFE', '#38D4C8', '#D43FBF'];
       Socket.emit("Message", newMessage); 
 
       $scope.message = '';    
+    }
+
+var gimmeEmojis = function(message)
+
+    { //(y)
+      message = message.replace(/;\)/g, "<img src='img/emoticons/1_27.png' width='20px' height='20px' />");
+      message = message.replace(/\(y\)/g, "<img src='img/emoticons/1_01.png' width='20px' height='20px' />");
+      message = message.replace(/O:\)/g, "<img src='img/emoticons/1_02.png' width='20px' height='20px' />");
+      message = message.replace(/:3/g, "<img src='img/emoticons/1_03.png' width='20px' height='20px' />");
+      message = message.replace(/o.O/g, "<img src='img/emoticons/1_04.png' width='20px' height='20px' />");
+      message = message.replace(/O.o/g, "<img src='img/emoticons/1_05.png' width='20px' height='20px' />");
+      message = message.replace(/:\'\(/g, "<img src='img/emoticons/1_06.png' width='20px' height='20px' />");
+      message = message.replace(/3:\)/g, "<img src='img/emoticons/1_07.png' width='20px' height='20px' />");
+      message = message.replace(/:\(/g, "<img src='img/emoticons/1_08.png' width='20px' height='20px' />");
+      message = message.replace(/:O/g, "<img src='img/emoticons/1_09.png' width='20px' height='20px' />");
+      message = message.replace(/8-\)/g, "<img src='img/emoticons/1_10.png' width='20px' height='20px' />");
+      message = message.replace(/:D/g, "<img src='img/emoticons/1_11.png' width='20px' height='20px' />");
+      message = message.replace(/>:\(/g, "<img src='img/emoticons/1_22.png' width='20px' height='20px' />");
+      message = message.replace(/\<3/g, "<img src='img/emoticons/1_13.png' width='20px' height='20px' />");
+      message = message.replace(/\^_\^/g, "<img src='img/emoticons/1_14.png' width='20px' height='20px' />");
+      message = message.replace(/\:\*/g, "<img src='img/emoticons/1_15.png' width='20px' height='20px' />");
+      message = message.replace(/\:v/g, "<img src='img/emoticons/1_16.png' width='20px' height='20px' />");
+      message = message.replace(/\<\(\"\)/g, "<img src='img/emoticons/1_17.png' width='20px' height='20px' />");
+      message = message.replace(/\:poop\:/g, "<img src='img/emoticons/1_18.png' width='20px' height='20px' />");
+      message = message.replace(/\:putnam\:/g, "<img src='img/emoticons/1_19.png' width='20px' height='20px' />");
+      message = message.replace(/\(\^\^\^\)/g, "<img src='img/emoticons/1_20.png' width='20px' height='20px' />");
+      message = message.replace(/\:\)/g, "<img src='img/emoticons/1_21.png' width='20px' height='20px' />");
+      message = message.replace(/\-\_\-/g, "<img src='img/emoticons/1_22.png' width='20px' height='20px' />");
+      message = message.replace(/8\|/g, "<img src='img/emoticons/1_23.png' width='20px' height='20px' />");
+      message = message.replace(/\:P/g, "<img src='img/emoticons/1_24.png' width='20px' height='20px' />");
+      message = message.replace(/\:\//g, "<img src='img/emoticons/1_25.png' width='20px' height='20px' />");
+      message = message.replace(/\>\:O/g, "<img src='img/emoticons/1_26.png' width='20px' height='20px' />");
+      message = message.replace(/\:\|\]/g, "<img src='img/emoticons/1_28.png' width='20px' height='20px' />");
+      
+      return message;
     }
 
     // user colors function
